cef662fe3e726523f18eb165ac6e3c80326313e4
ikabeee <carlglz30@gmail.com>
2024-11-10 19:47:15 -0500

    ADD: Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import SignIn from "./pages/auth/singin/SingIn"
+import SignUp from "./pages/auth/signup/SignUp"
+import Dashboard from "./pages/Dashboard/Dashboard"
+import ForgotYourPassword from "./pages/ForgotYourPassword/ForgotYoutPassword"
 
 function App() {
 
 
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<SignIn/>}/>
+        <Route path='/signUp' element={<SignUp/>}/>
+        <Route path='/forgotYourPassword' element={<ForgotYourPassword/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
       
     </>
   )
