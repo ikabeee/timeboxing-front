71750388c55cbf60422fc94ca195dd365bb6c83c
ikabeee <carlglz30@gmail.com>
2024-11-10 19:48:22 -0500

    ADD: User Interface

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -2,7 +2,8 @@ export default interface IUser{
     id?: number,
     name: string
     email: string,
-    password: string
+    password: string,
+    confirm_password: string,
     start_time: string,
     end_time: string,
     status: number
